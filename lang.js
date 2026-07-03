7eeb108b650ab504ff77fdefd7e9ef0f11a6e341
alizee-xia <131764313+alizee-xia@users.noreply.github.com>
2026-07-03 10:50:05 +0200

    Update language strings for home and about sections

--- lang.js
+++ lang.js
@@ -6,18 +6,24 @@ const data = {
     navProjects: "Projets",
     navLanguages: "Langues",
     navContact: "Contact",
+    
     homeTitle: "Bonjour, je suis Alizée XIA",
-    homeText: "Étudiante en Master Informatique – IoT",
+    homeText: "Future étudiante en Master 2 Informatique pour la Science des Données (ISD) – parcours IoT",
+    
     aboutTitle: "À propos",
-    aboutText: "Je suis étudiante en Master Informatique spécialisée en IoT et réseaux. Je développe en C++, Java, Python et Web.",
+    aboutText: "Future étudiante en Master 2 ISD (parcours IoT) à l'Université Paris-Saclay. Passionnée par le développement logiciel et la conception d'applications robustes (Java, C++, Python), je maîtrise la programmation orientée objet, le développement full-stack (PHP, JavaScript, SQL) ainsi que les environnements IoT et DevOps (Docker, Prometheus).",
+    
     skillsTitle: "Compétences",
+    
     projectsTitle: "Projets",
+    
     langTitle: "Langues",
-    contactTitle: "Contact",
     lang1: "Français – Bilingue",
     lang2: "Chinois – Bilingue",
     lang3: "Anglais – B2",
-    lang4: "Italien – A2–B1"
+    lang4: "Italien – A2–B1",
+
+    contactTitle: "Contact"
   },
   en: {
     navHome: "Home",
@@ -26,18 +32,25 @@ const data = {
     navProjects: "Projects",
     navLanguages: "Languages",
     navContact: "Contact",
+    
     homeTitle: "Hi, I'm Alizée XIA",
-    homeText: "Master student in Computer Science – IoT",
+    homeText: "Future Master 2 Computer Science for Data Science (ISD – IoT track) student",
+    
     aboutTitle: "About",
-    aboutText: "I am a Master student specialized in IoT and Networks. I develop in C++, Java, Python and Web.",
+    aboutText: "Future Master 2 ISD (IoT track) student at Université Paris-Saclay. Passionate about software development and building robust applications (Java, C++, Python), I master object-oriented programming, full-stack development (PHP, JavaScript, SQL) as well as IoT and DevOps environments (Docker, Prometheus).",
+    
     skillsTitle: "Skills",
+    
     projectsTitle: "Projects",
+    
     langTitle: "Languages",
-    contactTitle: "Contact",
     lang1: "French – Bilingual",
     lang2: "Chinese – Bilingual",
     lang3: "English – B2",
-    lang4: "Italian – A2–B1"
+    lang4: "Italian – A2–B1",
+
+    contactTitle: "Contact",
+    
   }
 }
 function setLang(l) {
